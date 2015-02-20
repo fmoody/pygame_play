b15c031fc7031f1e90ab121aa94abde8c003a582
Brains <fmoody@moodman.org>
2015-02-20 15:40:58 -0500

    Added horrible (simple)  ball collision handling, individual ball speeds, and time limiting the frame rate to 60fps

--- tutorial_2.py
+++ tutorial_2.py
@@ -5,8 +5,8 @@ import sys, pygame
 
 pygame.init()
 
-#size = width, height = 1680, 1050
-size = width, height = 640, 480
+size = width, height = 1680, 1050
+#size = width, height = 640, 640
 flags = 0
 
 screen = pygame.display.set_mode(size,flags)
@@ -14,12 +14,15 @@ screenrect = screen.get_rect()
 
 black = 0, 0, 0
 
-ball = pygame.transform.scale(pygame.image.load("ball.gif").convert_alpha(),[10,10])
-ballspeed = [2, 1]
+ball = pygame.transform.scale(pygame.image.load("ball.gif").convert_alpha(),[50,50])
+ballspeed = [1, 1]
+ballspeed_multiplier = 1
 
 balls = [] # Balls is a list of ball rects and directions
 
 paused = True
+
+clock = pygame.time.Clock()
 
 while 1:
     for event in pygame.event.get():
@@ -27,17 +30,16 @@ while 1:
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_f:
                 # Speed up the balls
-                ballspeed[0] = ballspeed[0]*2
-                ballspeed[1] = ballspeed[1]*2
+                ballspeed_multiplier *= 2
             elif event.key == pygame.K_s:
                 # Slow down the balls
-                ballspeed[0] = ballspeed[0]*0.5
-                ballspeed[1] = ballspeed[1]*0.5
+                ballspeed_multiplier *= 0.5
             elif event.key == pygame.K_p:
                 paused = not paused
+                print("Paused: " + str(paused))
             elif event.key == pygame.K_a:
                 # Add a ball
-                balls.append([ball.get_rect(), [1,1]])
+                balls.append([ball.get_rect(), ballspeed.copy()])
             elif event.key == pygame.K_d:
                 # Delete a ball
                 if len(balls) > 0:
@@ -47,23 +49,38 @@ while 1:
                 
     # Move balls
     if not paused:
-        for current_ball in balls:
-            current_balldirection = current_ball[1]
-            newrect = current_ball[0].move([current_ball[1][0]*ballspeed[0], current_ball[1][1]*ballspeed[1]] )
 
-            # Check Wall collisions
-            if screenrect.contains(newrect):
+        original_ball_info = balls.copy()
+
+        for index,current_ball in enumerate(balls):
+#            print("Index: " + str(index))
+            current_ballspeed = current_ball[1]
+            newrect = current_ball[0].move([current_ball[1][0]*ballspeed_multiplier, current_ball[1][1]*ballspeed_multiplier] )
+
+            # Check ball collisions (need to optimize this later)
+            remaining_balls_rects = [this_ball[0] for this_ball in original_ball_info]
+            collision_list = newrect.collidelistall(remaining_balls_rects)
+            if len(collision_list) > 1 :
+                collision_value  = [0,0]
+                for collision in collision_list:
+                    collision_value[0] += original_ball_info[collision][1][0]
+                    collision_value[1] += original_ball_info[collision][1][1]
+
+                current_ballspeed[0] = collision_value[0] / len(collision_list)
+                current_ballspeed[1] = collision_value[1] / len(collision_list)
+            elif screenrect.contains(newrect):             # Check Wall collisions. Update the ball if no collision.
                 current_ball[0] = newrect
-            else:
+            else: # Swap the sign of the x/y depending on the wall collided with.
                 if newrect.top < screenrect.top or newrect.bottom > screenrect.bottom:
-                    current_balldirection[1] = -current_balldirection[1]
+                    current_ballspeed[1] = -current_ballspeed[1]
                 if newrect.left < screenrect.left or newrect.right > screenrect.right:
-                    current_balldirection[0] = -current_balldirection[0]
+                    current_ballspeed[0] = -current_ballspeed[0]
 
     # Draw to surface
     screen.fill(black)
     for current_ball in balls:
         screen.blit(ball, current_ball[0])
     pygame.display.flip()
-    pygame.time.wait(1)
+    #pygame.time.wait(10)
+    clock.tick(60)
         
